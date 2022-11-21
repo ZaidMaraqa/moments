84b796c9cf32b7a14e163eb7f7089fed54f31e8d
Zaid Maraqa <zaid.maraqa@kcl.ac.uk>
2022-11-21 19:32:59 +0000

    fix the frontend issue

--- webapp/settings.py
+++ webapp/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- webapp/urls.py
+++ webapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from webapp.quickstart import views
+from django.views.genric import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -25,5 +26,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
