89d03a74531dabab38bc681067dc4868b293857c
Zaid Maraqa <zaid.maraqa@kcl.ac.uk>
2023-01-23 22:18:42 +0000

    tests for login and signup, also frontend for login and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import React from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+          <Routes>
+            <Route path="/" element={<HomePage/>} exact/>
+            <Route path="/login" element={<LoginPage/>}/>
+          </Routes>
+      </Router>
     </div>
   );
-}
+};
+
+// const appDiv = document.getElementById('app');
+// render(<App />, appDiv)
 
 export default App;
