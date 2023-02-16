fb9ac24fe3d08f0e11cbdb5c9db00d850f1122d8
Zaid Maraqa (k19063180) <k19063180@nmslab-004251.nms.kcl.ac.uk>
2023-02-16 13:22:35 +0000

    failed signup debugging

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,13 +1,44 @@
 import React, { useState, useContext } from 'react';
 import AuthContext from '../context/AuthContext'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
 
   const SignupPage = () => {
-  const { signUpUser } = useContext(AuthContext);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
+  // const { signUpUser } = useContext(AuthContext);
+  // const [username, setUsername] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password1, setPassword1] = useState('');
+  // const [password2, setPassword2] = useState('');
+  const navigate = useNavigate();
+  let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+  let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+
+  const signUpUser = async (e ) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8000/api/signup/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          'username': e.target.username.value,
+          'email': e.target.email.value,
+          'password1': e.target.password1.value,
+          'password2': e.target.password2.value,
+      }),
+    })
+    let data = await response.json();
+    if (response.status === 201) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem('authTokens', JSON.stringify(data));
+      navigate('/');
+    } 
+    else {
+      alert('Something went wrong :(');
+    }
+  };
 
   return (
     <div className="signup-container">
@@ -16,8 +47,8 @@ import { Link } from 'react-router-dom'
         <form onSubmit={signUpUser}>
           <input type="username" name="username" placeholder="Enter Username" />
           <input type="email" name="email" placeholder="Enter Email" />
-          <input type="password1 " name="password" placeholder="Enter Password" />
-          <input type="password" name="password" placeholder="Enter Password" />
+          <input type="password" name="password1" placeholder="Enter Password" />
+          <input type="password" name="password2" placeholder="Enter Password Confirmation" />
           <input type="submit" value="Submit" />
           <h5>Already have an account? Log in <Link to='/login'>here</Link></h5>
         </form>
